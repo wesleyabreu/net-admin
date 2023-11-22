be2e791af837e35daac5e5aa1d7ad3ef1f7407ff
Wesley Abreu <wesleyhenriquesantosabreu@gmail.com>
2023-11-22 17:16:30 -0300

    Adicionado models no CRUD do painel admin Django

--- src/AdminFinanceiro/admin.py
+++ src/AdminFinanceiro/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Plan
 
-# Register your models here.
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
+
+admin.site.register(Plan, PlanAdmin)
+
--- src/Atendente/admin.py
+++ src/Atendente/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Person, Contract, Schedule, Client
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("name", "cpf")
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Contract)
+admin.site.register(Schedule)
+admin.site.register(Client)
